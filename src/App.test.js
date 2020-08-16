bf39b9b1cd492fc097e3a3e05f7f0e263c83b323
Rungtiwa Inha <rungtiwa.in@baac.or.th>
2020-08-16 22:07:43 +0700

    add test case

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,29 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders Home Page link", () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/หน้าหลัก/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test("renders BAAC name (th) in Footer", () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/ธนาคารเพื่อการเกษตรและสหกรณ์การเกษตร/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test("renders BAAC name (en) in Footer", () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(
+    /Bank for Agriculture and Agricultural Cooperatives/i
+  );
+  expect(linkElement).toBeInTheDocument();
+});
+
+test("renders BAAC call center in Footer", () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/02-555-0555/i);
   expect(linkElement).toBeInTheDocument();
 });
